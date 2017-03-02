c6aaa427918b15deec21d847eabe258d0be2de7d
Steven Silvester <steven.silvester@ieee.org>
2017-03-02 16:34:03 -0600

    Finish dialog tests

--- test/dialog.spec.ts
+++ test/dialog.spec.ts
@@ -14,6 +14,10 @@ import {
 } from '@phosphor/messaging';
 
 import {
+  VirtualDOM, h
+} from '@phosphor/virtualdom';
+
+import {
   Widget
 } from '@phosphor/widgets';
 
@@ -393,14 +397,143 @@ describe('@jupyterlab/domutils', () => {
 
       describe('.defaultRenderer', () => {
 
+        it('should be an instance of a Renderer', () => {
+          expect(Dialog.defaultRenderer).to.be.an.instanceof(Dialog.Renderer);
+        });
+
       });
 
       describe('.Renderer', () => {
 
+        const renderer = Dialog.defaultRenderer;
+
+        const data: Dialog.IButton = {
+          label: 'foo',
+          icon: 'bar',
+          caption: 'hello',
+          className: 'baz',
+          action: 'reject',
+          displayType: 'warn'
+        };
+
+        describe('#createHeader()', () => {
+
+          it('should create the header of the dialog', () => {
+            let widget = renderer.createHeader('foo');
+            expect(widget.hasClass('jp-Dialog-header')).to.equal(true);
+            let node = widget.node.querySelector('.jp-Dialog-title');
+            expect(node.textContent).to.equal('foo');
+          });
+
+        });
+
+        describe('#createBody()', () => {
+
+          it('should create the body from a string', () => {
+            let widget = renderer.createBody('foo');
+            expect(widget.hasClass('jp-Dialog-body')).to.equal(true);
+            let node = widget.node.firstChild;
+            expect(node.textContent).to.equal('foo');
+          });
+
+          it('should create the body from an element', () => {
+            let vnode = h.div({}, [h.input(), h.select(), h.button()]);
+            let widget = renderer.createBody(VirtualDOM.realize(vnode));
+            let button = widget.node.querySelector('button');
+            expect(button.className).to.contain('jp-mod-styled');
+            let input = widget.node.querySelector('input');
+            expect(input.className).to.contain('jp-mod-styled');
+            let select = widget.node.querySelector('select');
+            expect(select.className).to.contain('jp-mod-styled');
+          });
+
+          it('should create the body from a widget', () => {
+            let body = new Widget();
+            renderer.createBody(body);
+            expect(body.hasClass('jp-Dialog-body')).to.equal(true);
+          });
+
+        });
+
+        describe('#createButtonNodes()', () => {
+
+          it('should create the button nodes of the dialog', () => {
+            let buttons = [Dialog.okButton, { label: 'foo' }] as Dialog.IButton[];
+            let nodes = renderer.createButtonNodes(buttons);
+            expect(nodes[0].className).to.contain('jp-Dialog-button');
+            expect(nodes[1].className).to.contain('jp-Dialog-button');
+          });
+
+        });
+
+        describe('#createFooter()', () => {
+
+          it('should create the footer of the dialog', () => {
+            let buttons = [Dialog.okButton, { label: 'foo' }] as Dialog.IButton[];
+            let nodes = renderer.createButtonNodes(buttons);
+            let footer = renderer.createFooter(nodes);
+            expect(footer.hasClass('jp-Dialog-footer')).to.equal(true);
+            expect(footer.node.contains(nodes[0])).to.equal(true);
+            expect(footer.node.contains(nodes[1])).to.equal(true);
+          });
+
+        });
+
+        describe('#renderButtonNode()', () => {
+
+          it('should render a button node for the dialog', () => {
+            let node = VirtualDOM.realize(renderer.renderButtonNode(data));
+            expect(node.className).to.contain('jp-Dialog-button');
+            expect(node.querySelector('.jp-Dialog-buttonIcon')).to.be.ok;
+            expect(node.querySelector('.jp-Dialog-buttonLabel')).to.be.ok;
+          });
+
+        });
+
+        describe('#renderIcon()', () => {
+
+          it('should render an icon element for a dialog item', () => {
+            let node = VirtualDOM.realize(renderer.renderIcon(data));
+            expect(node.className).to.contain('jp-Dialog-buttonIcon');
+          });
+
+        });
+
+        describe('#createItemClass()', () => {
+
+          it('should create the class name for the button', () => {
+            let value = renderer.createItemClass(data);
+            expect(value).to.contain('jp-Dialog-button');
+            expect(value).to.contain('jp-mod-reject');
+            expect(value).to.contain(data.className);
+          });
+
+        });
+
+        describe('#createIconClass()', () => {
+
+          it('should create the class name for the button icon', () => {
+            let value = renderer.createIconClass(data);
+            expect(value).to.contain('jp-Dialog-buttonIcon');
+            expect(value).to.contain(data.icon);
+          });
+
+        });
+
+        describe('#renderLabel()', () => {
+
+          it('should render a label element for a button', () => {
+            let node = VirtualDOM.realize(renderer.renderLabel(data));
+            expect(node.className).to.equal('jp-Dialog-buttonLabel');
+            expect(node.title).to.equal(data.caption);
+            expect(node.textContent).to.equal(data.label);
+          });
+
+        });
+
       });
 
     });
-
 
   });
 
