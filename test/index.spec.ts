844770473c74337478babf3c516cac3f873c7838
Steven Silvester <steven.silvester@ieee.org>
2017-03-02 08:03:12 -0600

    Add test infra

---
+++ test/index.spec.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import '@phosphor/widgets/style/index.css';
+
+import './dialog.spec';
+import './sanitizer.spec';
+import './vdom.spec';
